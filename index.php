a62e246d0aa7310a0ac306185c3a5494a725ba02
bramwaas <bram@waasdorpsoekhan.nl>
2017-09-22 17:46:15 +0200

    kleine aanpassing

--- index.php
+++ index.php
@@ -118,20 +118,13 @@ echo '<!-- base is ' . $doc->getBase() .' $templatestyleid ='.$templatestyleid .
 // Add extra metadata
 $doc->setMetaData( 'X-UA-Compatible', 'IE=edge', true ); // http-equiv = true 
 $doc->setMetaData( 'viewport', 'width=device-width, initial-scale=1.0' );
-// Add  Frameworks
-JHtml::_('bootstrap.framework');
 // Add Stylesheets
-//JHtmlBootstrap::loadCss();
-// Load optional rtl Bootstrap css and Bootstrap bugfixes
-//JHtmlBootstrap::loadCss($includeMaincss = false, $this->direction);
 $doc->addStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700');
 // bootstrap stylesheets van cdn
 $attribs = array('integrity' => 'sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u', 'crossorigin' => 'anonymous');
 $doc->addStyleSheet('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', 'text/css', null,  $attribs);
 $attribs = array('integrity' => 'sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp', 'crossorigin' => 'anonymous');
 $doc->addStyleSheet('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' , 'text/css', null, $attribs);
-// standaard bootstrap stijle
-// (for js $attribs = array('integrity' => 'sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u', 'crossorigin' => 'anonymous');
 // template stijl
 $doc->addStyleSheet('templates/' . $this->template . '/css/' . $wsaCssFilename);
 
