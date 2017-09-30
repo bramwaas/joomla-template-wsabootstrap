3270de5643bfabf5429c3af228f9dd50d617acba
bramwaas <bram@waasdorpsoekhan.nl>
2017-09-30 16:43:56 +0200

    override aangepast aan standaard

--- html/com_tags/tag/default_items.php
+++ html/com_tags/tag/default_items.php
@@ -9,6 +9,7 @@
 in deze component niet wordt gevonden 
    bw 19-10-2016 controle op leesmeer verbeterd omdat bij upgrade naar 3.6.3 (met display errors) bleek dat de oude versie een fout genereerde.(Notice: Trying to get property of non-object in /home/deb53453/domains/asha-s.com/public_html/templates/asha-s/html/com_tags/tag/default_items.php on line 121 als gevolg van $this->item->readmore ipv $this->item[0]->readmore)
    bw 14-04-2017 kleine aanpassingen na vgl v 3.7.0rc2
+   bw 30-09-2017 idem na vgl v3.8.0
  */
 
 defined('_JEXEC') or die;
@@ -23,95 +24,92 @@ $user = JFactory::getUser();
 
 // Check if user is allowed to add/edit based on tags permissions.
 // Do we really have to make it so people can see unpublished tags???
-$canEdit = $user->authorise('core.edit', 'com_tags');
-$canCreate = $user->authorise('core.create', 'com_tags');
+$canEdit      = $user->authorise('core.edit', 'com_tags');
+$canCreate    = $user->authorise('core.create', 'com_tags');
 $canEditState = $user->authorise('core.edit.state', 'com_tags');
-$items = $this->items;
-$n = count($this->items);
+$items        = $this->items;
+$n            = count($this->items);
+
 JFactory::getDocument()->addScriptDeclaration("
 		var resetFilter = function() {
 		document.getElementById('filter-search').value = '';
 	}
 ");
+
 ?>
 <form action="<?php echo htmlspecialchars(JUri::getInstance()->toString()); ?>" method="post" name="adminForm" id="adminForm" class="form-inline">
 	<?php if ($this->params->get('show_headings') || $this->params->get('filter_field') || $this->params->get('show_pagination_limit')) : ?>
-	<fieldset class="filters btn-toolbar">
-		<?php if ($this->params->get('filter_field')) : ?>
-			<div class="btn-group">
-				<label class="filter-search-lbl element-invisible" for="filter-search">
-					<?php echo JText::_('COM_TAGS_TITLE_FILTER_LABEL') . '&#160;'; ?>
-				</label>
-				<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('list.filter')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_TAGS_FILTER_SEARCH_DESC'); ?>" placeholder="<?php echo JText::_('COM_TAGS_TITLE_FILTER_LABEL'); ?>" />
-				<button type="button" name="filter-search-button" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>" onclick="document.adminForm.submit();" class="btn">
-					<span class="icon-search"></span>
-				</button>
-				<button type="reset" name="filter-clear-button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" class="btn" onclick="resetFilter(); document.adminForm.submit();">
-					<span class="icon-remove"></span>
-				</button>
-			</div>
-		<?php endif; ?>
-		<?php if ($this->params->get('show_pagination_limit')) : ?>
-			<div class="btn-group pull-right">
-				<label for="limit" class="element-invisible">
-					<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
-				</label>
-				<?php echo $this->pagination->getLimitBox(); ?>
-			</div>
-		<?php endif; ?>
-
-		<input type="hidden" name="filter_order" value="" />
-		<input type="hidden" name="filter_order_Dir" value="" />
-		<input type="hidden" name="limitstart" value="" />
-		<input type="hidden" name="task" value="" />
-		<div class="clearfix"></div>
-	</fieldset>
+		<fieldset class="filters btn-toolbar">
+			<?php if ($this->params->get('filter_field')) : ?>
+				<div class="btn-group">
+					<label class="filter-search-lbl element-invisible" for="filter-search">
+						<?php echo JText::_('COM_TAGS_TITLE_FILTER_LABEL') . '&#160;'; ?>
+					</label>
+					<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('list.filter')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_TAGS_FILTER_SEARCH_DESC'); ?>" placeholder="<?php echo JText::_('COM_TAGS_TITLE_FILTER_LABEL'); ?>" />
+					<button type="button" name="filter-search-button" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>" onclick="document.adminForm.submit();" class="btn">
+						<span class="icon-search"></span>
+					</button>
+					<button type="reset" name="filter-clear-button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" class="btn" onclick="resetFilter(); document.adminForm.submit();">
+						<span class="icon-remove"></span>
+					</button>
+				</div>
+			<?php endif; ?>
+			<?php if ($this->params->get('show_pagination_limit')) : ?>
+				<div class="btn-group pull-right">
+					<label for="limit" class="element-invisible">
+						<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
+					</label>
+					<?php echo $this->pagination->getLimitBox(); ?>
+				</div>
+			<?php endif; ?>
+			<input type="hidden" name="filter_order" value="" />
+			<input type="hidden" name="filter_order_Dir" value="" />
+			<input type="hidden" name="limitstart" value="" />
+			<input type="hidden" name="task" value="" />
+			<div class="clearfix"></div>
+		</fieldset>
 	<?php endif; ?>
-
-	<?php if ($this->items == false || $n == 0) : ?>
-		<p> <?php echo JText::_('COM_TAGS_NO_ITEMS'); ?></p>
+	<?php if ($this->items === false || $n === 0) : ?>
+		<p><?php echo JText::_('COM_TAGS_NO_ITEMS'); ?></p>
 	<?php else : ?>
-
-	<ul class="category list-striped">
-		<?php foreach ($items as $i => $item) : ?>
-			<?php if ($item->core_state == 0) : ?>
-				<li class="system-unpublished cat-list-row<?php echo $i % 2; ?>">
-			<?php else : ?>
-				<li class="cat-list-row<?php echo $i % 2; ?> clearfix" >
-				<?php if (($item->type_alias == 'com_users.category') || ($item->type_alias == 'com_banners.category')) : ?>
-					<h3>
-						<?php echo $this->escape($item->core_title); ?>
-					</h3>
+		<ul class="category list-striped">
+			<?php foreach ($items as $i => $item) : ?>
+				<?php if ($item->core_state == 0) : ?>
+					<li class="system-unpublished cat-list-row<?php echo $i % 2; ?>">
 				<?php else : ?>
-					<h3>
-						<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
+					<li class="cat-list-row<?php echo $i % 2; ?> clearfix">
+					<?php if (($item->type_alias === 'com_users.category') || ($item->type_alias === 'com_banners.category')) : ?>
+						<h3>
 							<?php echo $this->escape($item->core_title); ?>
-						</a>
-					</h3>
+						</h3>
+					<?php else : ?>
+						<h3>
+							<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
+								<?php echo $this->escape($item->core_title); ?>
+							</a>
+						</h3>
+					<?php endif; ?>
 				<?php endif; ?>
-			<?php endif; ?>
-			<?php // Content is generated by content plugin event "onContentAfterTitle" ?>
-			<?php echo $item->event->afterDisplayTitle; ?>
-			<?php $images  = json_decode($item->core_images); ?>
-			<?php if ($this->params->get('tag_list_show_item_image', 1) == 1 && !empty($images->image_intro)) : ?>
-				<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
-				<img src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>">
-				</a>
-			<?php endif; ?>
-			<?php if ($this->params->get('tag_list_show_item_description', 1)) : ?>
-				<?php // Content is generated by content plugin event "onContentBeforeDisplay" ?>
-				<?php echo $item->event->beforeDisplayContent; ?>
-				<span class="tag-body">
-					<?php echo JHtml::_('string.truncate', $item->core_body, $this->params->get('tag_list_item_maximum_characters')); ?>
-				</span>
-				<?php // Content is generated by content plugin event "onContentAfterDisplay" ?>
-				<?php echo $item->event->afterDisplayContent; ?>
-			<?php endif; ?>
-             
+				<?php // Content is generated by content plugin event "onContentAfterTitle" ?>
+				<?php echo $item->event->afterDisplayTitle; ?>
+				<?php $images = json_decode($item->core_images); ?>
+				<?php if ($this->params->get('tag_list_show_item_image', 1) == 1 && !empty($images->image_intro)) : ?>
+					<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
+						<img src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" />
+					</a>
+				<?php endif; ?>
+				<?php if ($this->params->get('tag_list_show_item_description', 1)) : ?>
+					<?php // Content is generated by content plugin event "onContentBeforeDisplay" ?>
+					<?php echo $item->event->beforeDisplayContent; ?>
+					<span class="tag-body">
+						<?php echo JHtml::_('string.truncate', $item->core_body, $this->params->get('tag_list_item_maximum_characters')); ?>
+					</span>
+					<?php // Content is generated by content plugin event "onContentAfterDisplay" ?>
+					<?php echo $item->event->afterDisplayContent; ?>
+				<?php endif; ?>
 
-                  <?php
-  /*   toegevoegd leesmeer bw als waarde van alternative_readmore is ingevuld, omdat readmore boolean waar ook op getest wordt uit item in deze component niet wordt gevonden */
-                  if ( (json_decode($item->core_params)->alternative_readmore > "")
+                 <?php //  toegevoegd leesmeer bw als waarde van alternative_readmore is ingevuld, omdat readmore boolean waar ook op getest wordt uit item in deze component niet wordt gevonden ?>
+                 <?php  if ( (json_decode($item->core_params)->alternative_readmore > "")
 			or (isset($this->item[0]->readmore) and $this->item[0]->readmore)
                               )  :
                   ?>
@@ -124,13 +122,10 @@ JFactory::getDocument()->addScriptDeclaration("
 			endif; ?>
               </a></p> 
                   
-<?php endif;  
- /*    toegevoeg leesmeer bw einde */              
-                  ?>  
+<?php endif;  // toegevoeg leesmeer bw einde ?>  
                   
 				</li>
-		<?php endforeach; ?>
-	</ul>
-
+			<?php endforeach; ?>
+		</ul>
 	<?php endif; ?>
 </form>
