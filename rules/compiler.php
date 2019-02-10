1cdfa641bb8240062494c3d64dbddb371abfe1c7
bram.waasdorp <bram.waasdorp@FNV-LT10086.FNV.NET>
2019-02-10 11:08:36 +0100

    1.4.4. test() vergelegen met v 4

--- rules/compiler.php
+++ rules/compiler.php
@@ -44,6 +44,9 @@ use Joomla\CMS\Uri\Uri;
 //use Joomla\CMS\HTML\HTMLHelper;
 //use Joomla\CMS\Language\Text;   // voor vertalingen???
 use Joomla\CMS\Form\FormRule;
+use Joomla\CMS\Form\Form;
+use Joomla\Registry\Registry;
+
 
  
 class WsaFormRuleCompiler extends FormRule
@@ -58,8 +61,24 @@ class WsaFormRuleCompiler extends FormRule
 
 //public function test(SimpleXMLElement $element, $value, $group = null, JRegistry $input = null, JForm $form = null)
 
-public function test(\SimpleXMLElement $element,  $value, string $group = null, \Joomla\Registry\Registry $input = null, \Joomla\CMS\Form\Form $form = null)
-
+    /**
+     * Method to test the value.
+     *
+     * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+     * @param   mixed              $value    The form field value to validate.
+     * @param   string             $group    The field name group control value. This acts as as an array container for the field.
+     *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+     *                                       full field name would end up being "bar[foo]".
+     * @param   Registry           $input    An optional Registry object with the entire data set to validate against the entire form.
+     * @param   Form               $form     The form object for which the field is being tested.
+     *
+     * @return  boolean  True if the value is valid, false otherwise.
+     *
+     * @since   1.7.0
+     * @throws  \UnexpectedValueException if rule is invalid.
+     */
+    public function test(\SimpleXMLElement $element, $value, $group = null, Registry $input = null, Form $form = null)
+    
     {
 
  $templatestyleid =  Uri::getInstance ()->getVar('id');
