73c5893cefe4c12e2d61ebf5fca4802f4d990d0b
ahcwa <ahcwa@bram-len.fritz.box>
2021-12-27 15:01:04 +0100

    2.0.2

--- index.php
+++ index.php
@@ -118,6 +118,12 @@ $this->setMetaData( 'viewport', 'width=device-width, initial-scale=1.0' );
 if ($joomlaverge4) { // Joomla 4 stylesheets and javascript via webassets
 // register overrides for older BS versions
 switch ($twbs_version) {
+    case "5" : {
+        $wa->registerStyle('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', ['version'=>'5.1.3'], ['integrity' => 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3', 'crossorigin' => 'anonymous'],[])
+        ->registerStyle('template.wsa_bootstrap', $wsaCssFilename, ['version' => $wsaTime], [],['bootstrap'])
+        ;
+        
+    }
     case "4" :  {
         $wa->registerStyle('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', ['version'=>'4.3.1'], ['integrity' => 'sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T', 'crossorigin' => 'anonymous'],[]);       
 //            $attribs = array('id'=>'bootstrap.min.css', 'integrity' => 'sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T', 'crossorigin' => 'anonymous');
@@ -137,7 +143,7 @@ switch ($twbs_version) {
 
 // Enable assets
     $wa->usePreset('template.wsa_bootstrap')
-    ->useStyle('template.active.language')
+    //    ->useStyle('template.wsa_bootstrap')
     ->addInlineScript('jQuery(document).ready(function() {
   jQuery(\'a[rel*="lightbox"], a[data-wsmodal]\').magnificPopup({
 type: \'image\'
